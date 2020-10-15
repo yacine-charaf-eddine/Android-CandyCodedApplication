457ff913895f151cf7d2c72a6e3f5f45ed5a8bc6
Bensouici Yacine Charaf Eddine <Piss096ff@gmail.com>
2020-10-15 08:32:09 +0100

    fix phoneIntent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,8 +41,8 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 3 - Launch the Phone Activity
     // ***
     public void createPhoneIntent(View view){
-        Uri phoneUri = Uri.parse("tel:0123456789");
-        Intent phoneIntent = new Intent(Intent.ACTION_DIAL,phoneUri);
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
         startActivity(phoneIntent);
     }
 }
