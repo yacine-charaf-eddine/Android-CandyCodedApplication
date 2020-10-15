76c020989d48ce669f0fb807c02f483d6e5ebe90
Bensouici Yacine Charaf Eddine <Piss096ff@gmail.com>
2020-10-15 08:27:15 +0100

    start the phone activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -25,6 +25,9 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
+    // ***
+    // TODO - Task 2 - Launch the Google Maps Activity
+    // ***
     public void createMapIntent(View view){
         Uri mapUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW,mapUri);
@@ -35,10 +38,11 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
-
-    // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Uri phoneUri = Uri.parse("tel:0123456789");
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL,phoneUri);
+        startActivity(phoneIntent);
+    }
 }
